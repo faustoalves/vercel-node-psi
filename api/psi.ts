a37be2c6904366c63453db16be2d6e14fe0a0731
Fausto Alves <fausto@faustoalves.com.br>
2024-01-24 16:17:27 -0300

    Change psi

--- api/psi.ts
+++ api/psi.ts
@@ -1,13 +1,14 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node'
-const psi = require('psi')
+import type {VercelRequest, VercelResponse} from "@vercel/node";
+const psi = require("psi");
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-  let { url, strategy } = req.query
-  const { data } = await psi(url, {
-    nokey: 'true',
+  let {url, strategy} = req.query;
+  console.log(url, strategy);
+  const {data} = await psi(url, {
+    nokey: "true",
     strategy: strategy,
-  })
+  });
   return res.json({
     performance: data.lighthouseResult.categories.performance.score,
-  })
+  });
 }
