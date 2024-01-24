bb7a4a30bc7348665ededb449b8ab52e4bbf4b3c
Fausto Alves <fausto@faustoalves.com.br>
2024-01-24 16:37:12 -0300

    Include Slack Message

--- api/psi.ts
+++ api/psi.ts
@@ -8,7 +8,28 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     nokey: "true",
     strategy: strategy,
   });
+  await sendMessageToSlack(
+    url,
+    `Strategy: ${strategy}, Performance: ${data.lighthouseResult.categories.performance.score}`
+  );
   return res.json({
     performance: data.lighthouseResult.categories.performance.score,
   });
 }
+
+const sendMessageToSlack = async (_url, message) => {
+  const url = "https://dev-api.benu.at/api/horus/error";
+  const body = {
+    resourceType: "BENU PSI",
+    resourceUrl: _url,
+    errorMessage: message,
+  };
+  const response = await fetch(url, {
+    method: "post",
+    body: JSON.stringify(body),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  console.log(response);
+};
